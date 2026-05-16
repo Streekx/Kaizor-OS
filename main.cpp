a46781ea99b04d2678d7714587f464ea3dd2aed2
Streekx <anshkumarchan@gmail.com>
2026-05-16 17:08:23 +0530

    Enhance KAIZOR OS startup sequence
    Refactor main function to improve startup messages and structure.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
 using namespace std;
 
+void systemInit() {
+    cout << "[CORE] Init Done" << endl;
+}
+
+class DesktopShell {
+public:
+    void load() {
+        cout << "[DESKTOP] Loaded" << endl;
+    }
+};
+
+class WindowManager {
+public:
+    void createWindow(string title) {
+        cout << "[WM] Window: " << title << endl;
+    }
+};
+
+class Compositor {
+public:
+    void init() {
+        cout << "[COMPOSITOR] Started" << endl;
+    }
+};
+
+class InputManager {
+public:
+    void listen() {
+        cout << "[INPUT] Active" << endl;
+    }
+};
+
 int main() {
-    cout << "KAIZOR OS STARTING..." << endl;
-    cout << "Loading Desktop Environment..." << endl;
+
+    cout << "====================" << endl;
+    cout << " KAIZOR OS START " << endl;
+    cout << "====================" << endl;
+
+    systemInit();
+
+    Compositor comp;
+    comp.init();
+
+    InputManager input;
+    input.listen();
+
+    DesktopShell desktop;
+    desktop.load();
+
+    WindowManager wm;
+    wm.createWindow("Files");
+    wm.createWindow("Browser");
+    wm.createWindow("Settings");
+
+    cout << "\n[KAIZOR OS READY]" << endl;
+
     return 0;
 }
