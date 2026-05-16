1248a87d250550f3ff685cb4233356a94891ffa4
Streekx <anshkumarchan@gmail.com>
2026-05-16 17:03:56 +0530

    Enhance WindowManager with new functionalities
    Added methods to focus, move, and render windows.

--- window_manager/window_manager.cpp
+++ window_manager/window_manager.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
+#include <vector>
+#include "window.h"
+
 using namespace std;
 
 class WindowManager {
+
+private:
+    vector<Window> windows;
+    int nextId = 1;
+
 public:
-    void createWindow(string name) {
-        cout << "Window Created: " << name << endl;
+
+    void createWindow(string title) {
+        Window w;
+        w.id = nextId++;
+        w.title = title;
+        w.x = 100;
+        w.y = 100;
+        w.width = 400;
+        w.height = 300;
+        w.focused = true;
+
+        windows.push_back(w);
+
+        cout << "[WM] Window Created: " << title << endl;
+    }
+
+    void focusWindow(int id) {
+        for (auto &w : windows)
+            w.focused = (w.id == id);
+
+        cout << "[WM] Focus changed to window " << id << endl;
+    }
+
+    void moveWindow(int id, int x, int y) {
+        for (auto &w : windows) {
+            if (w.id == id) {
+                w.x = x;
+                w.y = y;
+            }
+        }
+        cout << "[WM] Window moved" << endl;
+    }
+
+    void render() {
+        cout << "\n[WM] Rendering Windows..." << endl;
+
+        for (auto &w : windows) {
+            cout << "Window: " << w.title
+                 << " Pos(" << w.x << "," << w.y << ")"
+                 << (w.focused ? " [FOCUSED]" : "")
+                 << endl;
+        }
     }
 };
