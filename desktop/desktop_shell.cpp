5a62a495b004c83da79b6d5420820b188f397ff7
Streekx <anshkumarchan@gmail.com>
2026-05-16 17:05:22 +0530

    Add DesktopShell class to manage desktop interface

---
+++ desktop/desktop_shell.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class DesktopShell {
+
+public:
+
+    void load() {
+        cout << "[DESKTOP] Kaizor Desktop Loaded" << endl;
+        cout << "[DESKTOP] Dock: Files | Browser | Settings" << endl;
+        cout << "[DESKTOP] Wallpaper Applied" << endl;
+    }
+};
