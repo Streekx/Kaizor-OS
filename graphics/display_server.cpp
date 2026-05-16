08d7c5d2dc4e69cf41563be22338d23469089e77
Streekx <anshkumarchan@gmail.com>
2026-05-16 17:10:54 +0530

    Update display_server.cpp

--- graphics/display_server.cpp
+++ graphics/display_server.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+class DisplayServer {
+
+public:
+
+    void init() {
+        cout << "[DISPLAY] Server Started" << endl;
+        cout << "[DISPLAY] Screen Buffer Ready" << endl;
+    }
+
+    void createSurface(string name) {
+        cout << "[DISPLAY] Surface Created: " << name << endl;
+    }
+};
