8f791bee2a89b86c555233d704c5fe83f8717439
Streekx <anshkumarchan@gmail.com>
2026-05-17 16:14:50 +0530

    Update build_iso.sh

--- iso/build_iso.sh
+++ iso/build_iso.sh
@@ -1,1 +1,22 @@
 
+#!/bin/sh
+
+echo "====================================="
+echo " KAIZOR OS ISO BUILD SCRIPT"
+echo "====================================="
+
+OUTDIR="out"
+ROOTFS="rootfs"
+BOOTLOADER="bootloader"
+
+mkdir -p $OUTDIR
+
+echo "[KAIZOR] Packing rootfs..."
+tar -czf $OUTDIR/rootfs.tar.gz $ROOTFS
+
+echo "[KAIZOR] Copying bootloader..."
+cp -r $BOOTLOADER $OUTDIR/
+
+echo "[KAIZOR] Done."
+echo "NOTE: Real ISO build requires FreeBSD tools: makefs + mkimg"
+echo "Output stored in iso/out/"
