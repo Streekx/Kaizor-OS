771b65000dee53c9dc7c35595da7dd72fc3042d6
Streekx <anshkumarchan@gmail.com>
2026-05-17 18:02:01 +0530

    Add Renderer class for SDL rendering

---
+++ graphics/renderer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include "color.hpp"
+
+class Renderer {
+
+private:
+
+    SDL_Renderer* renderer;
+
+public:
+
+    Renderer();
+
+    bool initialize(
+        SDL_Window* window
+    );
+
+    void clear(
+        Color color
+    );
+
+    void drawRect(
+        int x,
+        int y,
+        int width,
+        int height,
+        Color color
+    );
+
+    void present();
+
+    SDL_Renderer* getSDLRenderer();
+
+    void shutdown();
+};
