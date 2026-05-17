e47987eca81a7a18c379f27b8aafc82bd155312c
Streekx <anshkumarchan@gmail.com>
2026-05-17 13:15:26 +0530

    Add StartMenu class header file

---
+++ gui/start_menu.h
@@ -0,0 +1,19 @@
+#ifndef START_MENU_H
+#define START_MENU_H
+
+class StartMenu {
+
+public:
+
+    bool opened;
+
+    StartMenu();
+
+    void open();
+
+    void close();
+
+    void render();
+};
+
+#endif
