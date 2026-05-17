b26f03d5bdeb8396b43207826dc3709aacbef15d
Streekx <anshkumarchan@gmail.com>
2026-05-17 14:58:25 +0530

    Implement start and stop methods for AudioServer

---
+++ audio/audio_server.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "audio_server.h"
+
+using namespace std;
+
+void AudioServer::start() {
+
+    cout << "[AUDIO] Server Started"
+         << endl;
+}
+
+void AudioServer::stop() {
+
+    cout << "[AUDIO] Server Stopped"
+         << endl;
+}
