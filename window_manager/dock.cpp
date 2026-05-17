b53435d4e8408a1280a8b25a0d8e1c548170815d
Streekx <anshkumarchan@gmail.com>
2026-05-17 10:06:57 +0530

    Implement Dock initialization and rendering methods

--- window_manager/dock.cpp
+++ window_manager/dock.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+#include "dock.h"
+
+using namespace std;
+
+void Dock::init() {
+
+    cout << "[DOCK] Started"
+         << endl;
+}
+
+void Dock::render() {
+
+    cout << "[DOCK] Icons Rendered"
+         << endl;
+}
