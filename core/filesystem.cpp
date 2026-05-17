c4c5cc474cab6509d1bc6306ada499926c140424
Streekx <anshkumarchan@gmail.com>
2026-05-17 11:28:37 +0530

    Implement basic FileSystem class methods

---
+++ core/filesystem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "filesystem.h"
+
+using namespace std;
+
+void FileSystem::mount() {
+
+    cout << "[FILESYSTEM] Root Mounted"
+         << endl;
+}
+
+void FileSystem::checkDisks() {
+
+    cout << "[FILESYSTEM] Disk Check Complete"
+         << endl;
+}
