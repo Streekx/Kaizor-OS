b8ea4e5611e5f447bdf0ff7d4b0916b32e1186e7
Streekx <anshkumarchan@gmail.com>
2026-05-17 10:41:25 +0530

    Implement basic animation functions

--- graphics/animations.cpp
+++ graphics/animations.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+#include "animations.h"
+
+using namespace std;
+
+void Animations::fadeIn() {
+
+    cout << "[ANIMATION] Fade In"
+         << endl;
+}
+
+void Animations::minimize() {
+
+    cout << "[ANIMATION] Minimize"
+         << endl;
+}
+
+void Animations::maximize() {
+
+    cout << "[ANIMATION] Maximize"
+         << endl;
+}
