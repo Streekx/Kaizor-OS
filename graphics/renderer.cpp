f08fb7b11dc293d5a3d75ba8e7508f4eb5b3dbcc
Streekx <anshkumarchan@gmail.com>
2026-05-17 18:03:50 +0530

    Refactor Renderer methods and initialization

--- graphics/renderer.cpp
+++ graphics/renderer.cpp
@@ -1,17 +1,98 @@
+#include "renderer.hpp"
+
 #include <iostream>
 
-#include "renderer.h"
+Renderer::Renderer() {
 
-using namespace std;
-
-void Renderer::init() {
-
-    cout << "[RENDERER] Started"
-         << endl;
+    renderer = nullptr;
 }
 
-void Renderer::drawFrame() {
+bool Renderer::initialize(
+    SDL_Window* window
+) {
 
-    cout << "[RENDERER] Frame Drawn"
-         << endl;
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+
+    if (!renderer) {
+
+        std::cout
+            << "[RENDERER] Failed"
+            << std::endl;
+
+        return false;
+    }
+
+    std::cout
+        << "[RENDERER] Initialized"
+        << std::endl;
+
+    return true;
+}
+
+void Renderer::clear(
+    Color color
+) {
+
+    SDL_SetRenderDrawColor(
+        renderer,
+        color.r,
+        color.g,
+        color.b,
+        color.a
+    );
+
+    SDL_RenderClear(renderer);
+}
+
+void Renderer::drawRect(
+    int x,
+    int y,
+    int width,
+    int height,
+    Color color
+) {
+
+    SDL_Rect rect = {
+
+        x,
+        y,
+        width,
+        height
+    };
+
+    SDL_SetRenderDrawColor(
+        renderer,
+        color.r,
+        color.g,
+        color.b,
+        color.a
+    );
+
+    SDL_RenderFillRect(
+        renderer,
+        &rect
+    );
+}
+
+void Renderer::present() {
+
+    SDL_RenderPresent(renderer);
+}
+
+SDL_Renderer* Renderer::getSDLRenderer() {
+
+    return renderer;
+}
+
+void Renderer::shutdown() {
+
+    SDL_DestroyRenderer(renderer);
+
+    std::cout
+        << "[RENDERER] Shutdown"
+        << std::endl;
 }
