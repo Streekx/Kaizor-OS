e077383ebb0a177283226a267a082c3e91848a52
Streekx <anshkumarchan@gmail.com>
2026-05-17 14:01:30 +0530

    Implement load and render methods in WidgetPanel

---
+++ gui/widget_panel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "widget_panel.h"
+
+using namespace std;
+
+void WidgetPanel::load() {
+
+    cout << "[WIDGET PANEL] Loaded"
+         << endl;
+}
+
+void WidgetPanel::render() {
+
+    cout << "[WIDGET PANEL] Rendered"
+         << endl;
+}
