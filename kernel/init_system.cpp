0a1ec2a8e0e44f803a5a650783f6fdbe2383a875
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:10:38 +0530

    Create init_system.cpp

---
+++ kernel/init_system.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "init_system.h"
+
+using namespace std;
+
+void InitSystem::loadServices() {
+
+    cout << "[INIT] Services Loaded"
+         << endl;
+}
+
+void InitSystem::startRuntime() {
+
+    cout << "[INIT] Runtime Started"
+         << endl;
+}
