dd122249499aaee4f3f57d0c5a0628409b519fc1
Streekx <anshkumarchan@gmail.com>
2026-05-17 09:56:35 +0530

    Add Compositor class initialization and composition methods

--- graphics/compositor.cpp
+++ graphics/compositor.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+#include "compositor.h"
+
+using namespace std;
+
+void Compositor::init() {
+
+    cout << "[COMPOSITOR] Started"
+         << endl;
+}
+
+void Compositor::compose() {
+
+    cout << "[COMPOSITOR] Layers Composed"
+         << endl;
+}
