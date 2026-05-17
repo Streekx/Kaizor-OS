0f6765e396a9de031a657a8dcf8592d129955058
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:19:16 +0530

    Add AnimationEngine class header file

---
+++ gui/animation_engine.h
@@ -0,0 +1,15 @@
+#ifndef ANIMATION_ENGINE_H
+#define ANIMATION_ENGINE_H
+
+class AnimationEngine {
+
+public:
+
+    void start();
+
+    void animateWindows();
+
+    void animateDock();
+};
+
+#endif
