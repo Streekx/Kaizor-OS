afaee6b7d8c496901c6f47575a3b449f650e2c5e
Streekx <anshkumarchan@gmail.com>
2026-05-17 11:22:35 +0530

    Add splash screen display functionality

--- boot/splash.cpp
+++ boot/splash.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+#include "splash.h"
+
+using namespace std;
+
+void Splash::show() {
+
+    cout << "[SPLASH] Kaizor Logo Displayed"
+         << endl;
+
+    cout << "[SPLASH] Forest Wallpaper Loaded"
+         << endl;
+}
