1a0b5b396eb96593e5d7d2fbbc03a6bbf84f1939
Streekx <anshkumarchan@gmail.com>
2026-05-17 10:06:03 +0530

    Add taskbar initialization and rendering methods

--- window_manager/taskbar.cpp
+++ window_manager/taskbar.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+#include "taskbar.h"
+
+using namespace std;
+
+void Taskbar::init() {
+
+    cout << "[TASKBAR] Started"
+         << endl;
+}
+
+void Taskbar::render() {
+
+    cout << "[TASKBAR] Rendered"
+         << endl;
+
+    cout << "[TASKBAR] Apps: Files Browser Settings"
+         << endl;
+}
