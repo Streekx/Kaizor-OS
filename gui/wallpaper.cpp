4d3c593fc6225b0233afb8ab108627b8e280afa9
Streekx <anshkumarchan@gmail.com>
2026-05-17 13:59:20 +0530

    Implement Wallpaper class with load and render methods

---
+++ gui/wallpaper.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "wallpaper.h"
+
+using namespace std;
+
+void Wallpaper::load() {
+
+    cout << "[WALLPAPER] Loaded"
+         << endl;
+}
+
+void Wallpaper::render() {
+
+    cout << "[WALLPAPER] Rendered"
+         << endl;
+}
