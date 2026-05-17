4e894111e245742803a22ffe5e3ba656316addb6
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:17:38 +0530

    Implement BlurEngine with initialization and blur methods

---
+++ gui/blur_engine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "blur_engine.h"
+
+using namespace std;
+
+void BlurEngine::initialize() {
+
+    cout << "[BLUR] Engine Initialized"
+         << endl;
+}
+
+void BlurEngine::applyBlur() {
+
+    cout << "[BLUR] Glass Blur Applied"
+         << endl;
+}
