735a5025eb34aa59885c0ea21746afca9dedcf6b
Streekx <anshkumarchan@gmail.com>
2026-05-17 10:39:47 +0530

    Update workspace.cpp

--- window_manager/workspace.cpp
+++ window_manager/workspace.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+#include "workspace.h"
+
+using namespace std;
+
+void Workspace::init() {
+
+    cout << "[WORKSPACE] Manager Started"
+         << endl;
+}
+
+void Workspace::switchWorkspace(int id) {
+
+    cout << "[WORKSPACE] Switched To "
+         << id
+         << endl;
+}
