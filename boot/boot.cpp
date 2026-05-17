c5a6a36502799f56e0f93f434c59d1dc26eb94d3
Streekx <anshkumarchan@gmail.com>
2026-05-17 11:21:56 +0530

    Update boot.cpp

--- boot/boot.cpp
+++ boot/boot.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+#include "boot.h"
+
+using namespace std;
+
+void Boot::start() {
+
+    cout << "[BOOT] Kaizor Boot Started"
+         << endl;
+
+    cout << "[BOOT] Loading Core Services"
+         << endl;
+}
