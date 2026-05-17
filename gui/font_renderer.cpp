20e50631c57a8427dcae98ae230b6a5c6bac0e3b
Streekx <anshkumarchan@gmail.com>
2026-05-17 13:12:38 +0530

    Implement FontRenderer with load and render methods

---
+++ gui/font_renderer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "font_renderer.h"
+
+using namespace std;
+
+void FontRenderer::loadFonts() {
+
+    cout << "[FONT] Loaded"
+         << endl;
+}
+
+void FontRenderer::renderText() {
+
+    cout << "[FONT] Text Rendered"
+         << endl;
+}
