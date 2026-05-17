f7a9dc01b55e1029e430a0889916a782958f70cb
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:25:43 +0530

    Add DesktopRenderer class with rendering methods

---
+++ graphics/desktop_renderer.h
@@ -0,0 +1,15 @@
+#ifndef DESKTOP_RENDERER_H
+#define DESKTOP_RENDERER_H
+
+class DesktopRenderer {
+
+public:
+
+    void renderWallpaper();
+
+    void renderDock();
+
+    void renderTaskbar();
+};
+
+#endif
