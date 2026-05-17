5ee13e4c304ee20c5e3818bc1f4c405b0c7e10db
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:27:10 +0530

    Implement window rendering and shadow drawing

---
+++ graphics/window_renderer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "window_renderer.h"
+
+using namespace std;
+
+void WindowRenderer::drawWindow() {
+
+    cout << "[WINDOW] Rendered"
+         << endl;
+}
+
+void WindowRenderer::drawShadow() {
+
+    cout << "[WINDOW] Shadow Drawn"
+         << endl;
+}
