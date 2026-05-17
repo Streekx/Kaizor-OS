50be58907c33d3826462a124dd387c2a8b25faba
Streekx <anshkumarchan@gmail.com>
2026-05-17 14:32:44 +0530

    Implement wallpaper loading and animation functions

---
+++ desktop/wallpaper_engine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "wallpaper_engine.h"
+
+using namespace std;
+
+void WallpaperEngine::loadWallpaper() {
+
+    cout << "[WALLPAPER ENGINE] Wallpaper Loaded"
+         << endl;
+}
+
+void WallpaperEngine::animateWallpaper() {
+
+    cout << "[WALLPAPER ENGINE] Animation Running"
+         << endl;
+}
