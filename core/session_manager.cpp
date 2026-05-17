9163e1294d4ec72af1399f61860922c2520d7d8c
Streekx <anshkumarchan@gmail.com>
2026-05-17 11:26:04 +0530

    Implement startSession method in SessionManager

---
+++ core/session_manager.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "session_manager.h"
+
+using namespace std;
+
+void SessionManager::startSession() {
+
+    cout << "[SESSION] Kaizor Desktop Session Started"
+         << endl;
+}
