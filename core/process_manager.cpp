e155f72c70a09366abb4f7d32699c3cd895c114b
Streekx <anshkumarchan@gmail.com>
2026-05-17 11:11:34 +0530

    Implement process manager initialization and start

--- core/process_manager.cpp
+++ core/process_manager.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+#include "process_manager.h"
+
+using namespace std;
+
+void ProcessManager::init() {
+
+    cout << "[PROCESS] Manager Started"
+         << endl;
+}
+
+void ProcessManager::startProcess(
+    const char* name
+) {
+
+    cout << "[PROCESS] Running: "
+         << name
+         << endl;
+}
