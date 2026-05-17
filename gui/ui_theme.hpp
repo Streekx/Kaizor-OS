7b0450a4e4f5f2112cbe0425e0967591d2c65de1
Streekx <anshkumarchan@gmail.com>
2026-05-17 18:32:47 +0530

    Add UITheme class for theme color management

---
+++ gui/ui_theme.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../graphics/color.hpp"
+
+class UITheme {
+
+public:
+
+    static Color wallpaper();
+
+    static Color taskbar();
+
+    static Color dock();
+
+    static Color focusedWindow();
+
+    static Color normalWindow();
+
+    static Color titlebar();
+
+    static Color accent();
+};
