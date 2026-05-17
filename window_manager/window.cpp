ddedaf05dc7e4e8c87155f75bb0a2a354bc748ba
Streekx <anshkumarchan@gmail.com>
2026-05-17 09:19:01 +0530

    Add constructor for Window class
    Implement constructor for Window class to initialize properties.

--- window_manager/window.cpp
+++ window_manager/window.cpp
@@ -1,1 +1,23 @@
+#include "window.h"
 
+Window::Window(
+    int id,
+    std::string title,
+    int x,
+    int y,
+    int width,
+    int height
+) {
+
+    this->id = id;
+
+    this->title = title;
+
+    this->x = x;
+    this->y = y;
+
+    this->width = width;
+    this->height = height;
+
+    this->focused = false;
+}
