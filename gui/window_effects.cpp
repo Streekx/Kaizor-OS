f03e135bd3c5a503e337715b2303e2ca60b04e96
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:20:32 +0530

    Implement window effects for shadows and corners

---
+++ gui/window_effects.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "window_effects.h"
+
+using namespace std;
+
+void WindowEffects::shadows() {
+
+    cout << "[WINDOW EFFECTS] Shadows Enabled"
+         << endl;
+}
+
+void WindowEffects::roundedCorners() {
+
+    cout << "[WINDOW EFFECTS] Rounded Corners Enabled"
+         << endl;
+}
