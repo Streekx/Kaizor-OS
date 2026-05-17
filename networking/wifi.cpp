7a528107430ad7c827f8e73f0fe8f8efc41ead24
Streekx <anshkumarchan@gmail.com>
2026-05-17 14:45:13 +0530

    Implement WiFi class with scan, connect, and status methods

---
+++ networking/wifi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "wifi.h"
+
+using namespace std;
+
+void WiFi::scan() {
+
+    cout << "[WIFI] Scanning Networks"
+         << endl;
+}
+
+void WiFi::connect() {
+
+    cout << "[WIFI] Connected"
+         << endl;
+}
+
+void WiFi::status() {
+
+    cout << "[WIFI] Signal Excellent"
+         << endl;
+}
