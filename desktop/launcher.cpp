d59eb3cff7d39a34eb6c8fb6c26a78da8218f4a0
Streekx <anshkumarchan@gmail.com>
2026-05-17 10:09:12 +0530

    Implement init and open methods in Launcher

--- desktop/launcher.cpp
+++ desktop/launcher.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+#include "launcher.h"
+
+using namespace std;
+
+void Launcher::init() {
+
+    cout << "[LAUNCHER] Ready"
+         << endl;
+}
+
+void Launcher::open() {
+
+    cout << "[LAUNCHER] Applications Opened"
+         << endl;
+}
