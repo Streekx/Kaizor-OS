79b5445ae93d1386827909d0ff19856c5ba415f4
Streekx <anshkumarchan@gmail.com>
2026-05-17 11:24:18 +0530

    Implement login functionality in LoginManager

--- boot/login_manager.cpp
+++ boot/login_manager.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+#include "login_manager.h"
+
+using namespace std;
+
+void LoginManager::login() {
+
+    cout << "[LOGIN] User Authenticated"
+         << endl;
+
+    cout << "[LOGIN] Session Started"
+         << endl;
+}
