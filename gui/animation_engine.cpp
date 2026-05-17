8d715f6af174e1f7b2a91265725394103ac8a4bc
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:19:44 +0530

    Implement basic animation engine functionality

---
+++ gui/animation_engine.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "animation_engine.h"
+
+using namespace std;
+
+void AnimationEngine::start() {
+
+    cout << "[ANIMATION] Engine Started"
+         << endl;
+}
+
+void AnimationEngine::animateWindows() {
+
+    cout << "[ANIMATION] Window Effects Active"
+         << endl;
+}
+
+void AnimationEngine::animateDock() {
+
+    cout << "[ANIMATION] Dock Animation Active"
+         << endl;
+}
