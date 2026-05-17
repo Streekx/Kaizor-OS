e88d962eca0d3d1133dd71f700e4f70edc3914f7
Streekx <anshkumarchan@gmail.com>
2026-05-17 09:20:06 +0530

    Refactor WindowManager methods and remove unused code

--- window_manager/window_manager.cpp
+++ window_manager/window_manager.cpp
@@ -1,57 +1,53 @@
 #include <iostream>
-#include <vector>
-#include "window.h"
+
+#include "window_manager.h"
 
 using namespace std;
 
-class WindowManager {
+void WindowManager::createWindow(
+    int id,
+    string title,
+    int x,
+    int y,
+    int width,
+    int height
+) {
 
-private:
-    vector<Window> windows;
-    int nextId = 1;
+    Window w(
+        id,
+        title,
+        x,
+        y,
+        width,
+        height
+    );
 
-public:
+    windows.push_back(w);
 
-    void createWindow(string title) {
-        Window w;
-        w.id = nextId++;
-        w.title = title;
-        w.x = 100;
-        w.y = 100;
-        w.width = 400;
-        w.height = 300;
-        w.focused = true;
+    cout << "[WM] Window Created: "
+         << title << endl;
+}
 
-        windows.push_back(w);
+void WindowManager::render() {
 
-        cout << "[WM] Window Created: " << title << endl;
+    cout << endl;
+
+    cout << "====== WINDOWS ======" << endl;
+
+    for (auto &w : windows) {
+
+        cout << w.title
+             << " Pos("
+             << w.x
+             << ","
+             << w.y
+             << ") Size("
+             << w.width
+             << "x"
+             << w.height
+             << ")"
+             << endl;
     }
 
-    void focusWindow(int id) {
-        for (auto &w : windows)
-            w.focused = (w.id == id);
-
-        cout << "[WM] Focus changed to window " << id << endl;
-    }
-
-    void moveWindow(int id, int x, int y) {
-        for (auto &w : windows) {
-            if (w.id == id) {
-                w.x = x;
-                w.y = y;
-            }
-        }
-        cout << "[WM] Window moved" << endl;
-    }
-
-    void render() {
-        cout << "\n[WM] Rendering Windows..." << endl;
-
-        for (auto &w : windows) {
-            cout << "Window: " << w.title
-                 << " Pos(" << w.x << "," << w.y << ")"
-                 << (w.focused ? " [FOCUSED]" : "")
-                 << endl;
-        }
-    }
-};
+    cout << "=====================" << endl;
+}
