af1a4d6de2557a6380497034d1cfef148a77931d
Streekx <anshkumarchan@gmail.com>
2026-05-17 09:19:29 +0530

    Add WindowManager class definition with methods

--- window_manager/window_manager.h
+++ window_manager/window_manager.h
@@ -1,1 +1,27 @@
+#ifndef WINDOW_MANAGER_H
+#define WINDOW_MANAGER_H
 
+#include <vector>
+#include "window.h"
+
+class WindowManager {
+
+private:
+
+    std::vector<Window> windows;
+
+public:
+
+    void createWindow(
+        int id,
+        std::string title,
+        int x,
+        int y,
+        int width,
+        int height
+    );
+
+    void render();
+};
+
+#endif
