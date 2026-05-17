5af816971e453f5e8c0ffe1bad51c39a3c17933e
Streekx <anshkumarchan@gmail.com>
2026-05-17 15:24:16 +0530

    Implement GUIRenderer with initialization and rendering

---
+++ graphics/gui_renderer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "gui_renderer.h"
+
+using namespace std;
+
+void GUIRenderer::initialize() {
+
+    cout << "[GUI] Renderer Initialized"
+         << endl;
+}
+
+void GUIRenderer::renderFrame() {
+
+    cout << "[GUI] Rendering Frame"
+         << endl;
+}
