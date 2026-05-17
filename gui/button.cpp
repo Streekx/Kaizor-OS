203ed42c2649fe7d659d6875250c29d43ee532a4
Streekx <anshkumarchan@gmail.com>
2026-05-17 12:01:15 +0530

    Implement Button class with rendering and clicking

---
+++ gui/button.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "button.h"
+
+using namespace std;
+
+Button::Button(
+    string label,
+    int x,
+    int y,
+    int width,
+    int height
+) {
+
+    this->label = label;
+
+    this->x = x;
+    this->y = y;
+
+    this->width = width;
+    this->height = height;
+}
+
+void Button::render() {
+
+    cout << "[BUTTON] "
+         << label
+         << " Rendered"
+         << endl;
+}
+
+void Button::click() {
+
+    cout << "[BUTTON] "
+         << label
+         << " Clicked"
+         << endl;
+}
