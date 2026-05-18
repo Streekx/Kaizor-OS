6bf40b444a631b07b632b674bf7fdaccdd45857c
Streekx <anshkumarchan@gmail.com>
2026-05-18 21:10:25 +0530

    Change AppLauncher to render UI elements
    Refactor AppLauncher to render UI components instead of launching apps.

--- gui/app_launcher.cpp
+++ gui/app_launcher.cpp
@@ -1,14 +1,89 @@
-#include <iostream>
-
 #include "app_launcher.h"
 
-using namespace std;
-
-void AppLauncher::launch(
-    const char* app
+void AppLauncher::render(
+    Renderer& renderer
 ) {
 
-    cout << "[APP] Launching: "
-         << app
-         << endl;
+    /* PANEL */
+
+    renderer.drawRoundedRect(
+        340,
+        110,
+        600,
+        470,
+        28,
+        Color(
+            18,
+            22,
+            38,
+            235
+        )
+    );
+
+    /* SEARCH BAR */
+
+    renderer.drawRoundedRect(
+        390,
+        140,
+        500,
+        44,
+        14,
+        Color(
+            36,
+            42,
+            66,
+            255
+        )
+    );
+
+    /* APPS GRID */
+
+    int startX = 390;
+    int startY = 220;
+
+    int x = startX;
+    int y = startY;
+
+    for (
+        int i = 0;
+        i < 12;
+        i++
+    ) {
+
+        renderer.drawRoundedRect(
+            x,
+            y,
+            90,
+            90,
+            22,
+            Color(
+                60,
+                110,
+                255,
+                220
+            )
+        );
+
+        renderer.drawRoundedRect(
+            x + 18,
+            y + 18,
+            54,
+            54,
+            16,
+            Color(
+                220,
+                235,
+                255,
+                120
+            )
+        );
+
+        x += 120;
+
+        if ((i + 1) % 4 == 0) {
+
+            x = startX;
+            y += 120;
+        }
+    }
 }
