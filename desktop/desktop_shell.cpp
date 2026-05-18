9b19f81acc90dc5cc20573a07d7da3b98e4c65fa
Streekx <anshkumarchan@gmail.com>
2026-05-18 20:39:05 +0530

    Improve formatting of render method in DesktopShell
    Refactor render method for better readability and structure.

--- desktop/desktop_shell.cpp
+++ desktop/desktop_shell.cpp
@@ -1,18 +1,17 @@
 #include "desktop_shell.hpp"
-#include "../graphics/display_server.hpp"
 
 DesktopShell::DesktopShell() {
 }
 
-void DesktopShell::render(Renderer& renderer) {
+void DesktopShell::render(
+    Renderer& renderer
+) {
 
-    // Load wallpaper only once (lazy init)
-    static bool wallpaperLoaded = false;
-    if (!wallpaperLoaded) {
-        wallpaper.loadWallpaper(renderer.getSDLRenderer());
-        wallpaperLoaded = true;
-    }
+    wallpaper.render(
+        renderer
+    );
 
-    wallpaper.render(renderer);
-    icons.render(renderer);
+    icons.render(
+        renderer
+    );
 }
