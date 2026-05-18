f6868fb65d561ae9735827758dc2e4440b9149e7
Streekx <anshkumarchan@gmail.com>
2026-05-18 21:08:50 +0530

    Refactor login function to render UI elements
    Refactor login function to render UI elements for login panel, avatar, user field, and password field.

--- boot/login_manager.cpp
+++ boot/login_manager.cpp
@@ -1,14 +1,70 @@
-#include <iostream>
-
 #include "login_manager.h"
 
-using namespace std;
+void LoginManager::render(
+    Renderer& renderer
+) {
 
-void LoginManager::login() {
+    /* PANEL */
 
-    cout << "[LOGIN] User Authenticated"
-         << endl;
+    renderer.drawRoundedRect(
+        430,
+        160,
+        420,
+        380,
+        28,
+        Color(
+            18,
+            22,
+            36,
+            235
+        )
+    );
 
-    cout << "[LOGIN] Session Started"
-         << endl;
+    /* AVATAR */
+
+    renderer.drawRoundedRect(
+        560,
+        200,
+        160,
+        160,
+        80,
+        Color(
+            90,
+            140,
+            255,
+            255
+        )
+    );
+
+    /* USER FIELD */
+
+    renderer.drawRoundedRect(
+        500,
+        410,
+        280,
+        46,
+        14,
+        Color(
+            40,
+            48,
+            72,
+            255
+        )
+    );
+
+    /* PASSWORD FIELD */
+
+    renderer.drawRoundedRect(
+        500,
+        470,
+        280,
+        46,
+        14,
+        Color(
+            40,
+            48,
+            72,
+            255
+        )
+    );
 }
