bcb73eb53f8441f8159ce209b26e05cf6e456f2e
Streekx <anshkumarchan@gmail.com>
2026-05-18 12:51:54 +0530

    Refactor Window constructor parameters and initialization

--- window_manager/window.hpp
+++ window_manager/window.hpp
@@ -7,28 +7,41 @@ class Window {
 public:
 
     int id;
-
     std::string title;
 
     int x;
     int y;
-
     int width;
     int height;
 
     bool focused;
 
     bool dragging;
-
     int dragOffsetX;
     int dragOffsetY;
 
     Window(
-        int windowId,
-        const std::string& windowTitle,
+        int winId,
+        const std::string& winTitle,
         int posX,
         int posY,
         int w,
         int h
-    );
+    ) {
+
+        id = winId;
+        title = winTitle;
+
+        x = posX;
+        y = posY;
+
+        width = w;
+        height = h;
+
+        focused = false;
+        dragging = false;
+
+        dragOffsetX = 0;
+        dragOffsetY = 0;
+    }
 };
