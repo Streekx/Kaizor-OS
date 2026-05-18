90aa649a35f8fc7f8ca3b2ab309c5e7198c5540c
Streekx <anshkumarchan@gmail.com>
2026-05-18 19:46:04 +0530

    Update font_manager.hpp

--- gui/font_manager.hpp
+++ gui/font_manager.hpp
@@ -3,19 +3,13 @@
 #include <SDL2/SDL_ttf.h>
 
 class FontManager {
-
 private:
-
     TTF_Font* font;
 
 public:
-
     FontManager();
 
-    bool initialize(
-        const char* path,
-        int size
-    );
+    bool initialize(const char* path, int size);
 
     TTF_Font* getFont();
 
