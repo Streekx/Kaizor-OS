da9cda2e64a5c8cf4c35ef6d95fadb1bca71752d
Streekx <anshkumarchan@gmail.com>
2026-05-18 19:28:52 +0530

    Refactor Renderer methods and add drawTexture
    Refactored method signatures for clarity and added texture drawing support.

--- graphics/renderer.hpp
+++ graphics/renderer.hpp
@@ -1,43 +1,25 @@
 #pragma once
 
 #include <SDL2/SDL.h>
-
 #include "color.hpp"
 
 class Renderer {
-
 private:
-
     SDL_Renderer* renderer;
 
 public:
-
     Renderer();
 
-    bool initialize(
-        SDL_Window* window
-    );
+    bool initialize(SDL_Window* window);
 
-    void clear(
-        Color color
-    );
+    void clear(Color color);
 
-    void drawRect(
-        int x,
-        int y,
-        int width,
-        int height,
-        Color color
-    );
+    void drawRect(int x, int y, int width, int height, Color color);
 
-    void drawRoundedRect(
-        int x,
-        int y,
-        int width,
-        int height,
-        int radius,
-        Color color
-    );
+    void drawRoundedRect(int x, int y, int width, int height, int radius, Color color);
+
+    // NEW: texture draw support (needed for wallpaper)
+    void drawTexture(SDL_Texture* texture, int x, int y, int width, int height);
 
     void present();
 
