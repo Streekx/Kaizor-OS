8df2e212ef9281379fdf502b3032583462262927
Streekx <anshkumarchan@gmail.com>
2026-05-18 21:11:37 +0530

    Update NotificationCenter to use renderer for notifications
    Refactor NotificationCenter methods to use renderer for drawing notifications.

--- desktop/notification_center.cpp
+++ desktop/notification_center.cpp
@@ -1,20 +1,45 @@
-#include <iostream>
-
 #include "notification_center.h"
 
-using namespace std;
-
-void NotificationCenter::open() {
-
-    cout << "[NOTIFICATION CENTER] Opened"
-         << endl;
-}
-
-void NotificationCenter::push(
-    const char* text
+void NotificationCenter::render(
+    Renderer& renderer
 ) {
 
-    cout << "[NOTIFICATION] "
-         << text
-         << endl;
+    renderer.drawRoundedRect(
+        980,
+        70,
+        260,
+        420,
+        24,
+        Color(
+            18,
+            24,
+            42,
+            230
+        )
+    );
+
+    int y = 110;
+
+    for (
+        int i = 0;
+        i < 4;
+        i++
+    ) {
+
+        renderer.drawRoundedRect(
+            1005,
+            y,
+            210,
+            70,
+            18,
+            Color(
+                40,
+                52,
+                88,
+                255
+            )
+        );
+
+        y += 92;
+    }
 }
