fc56d34080edca5c93987bddf2d3a6a846434d4e
Streekx <anshkumarchan@gmail.com>
2026-05-18 21:16:41 +0530

    Implement render method in TerminalApp
    Refactor TerminalApp to implement render method for UI.

--- apps/terminal_app.cpp
+++ apps/terminal_app.cpp
@@ -1,10 +1,101 @@
 #include "terminal_app.hpp"
 
-#include <iostream>
+void TerminalApp::render(
+    Renderer& renderer
+) {
 
-void TerminalApp::launch() {
+    /* TERMINAL BODY */
 
-    std::cout
-        << "[APP] Terminal Started"
-        << std::endl;
+    renderer.drawRoundedRect(
+        220,
+        120,
+        840,
+        500,
+        22,
+        Color(
+            10,
+            12,
+            18,
+            245
+        )
+    );
+
+    /* TITLEBAR */
+
+    renderer.drawRoundedRect(
+        220,
+        120,
+        840,
+        42,
+        22,
+        Color(
+            24,
+            28,
+            44,
+            255
+        )
+    );
+
+    /* BUTTONS */
+
+    renderer.drawRect(
+        240,
+        136,
+        10,
+        10,
+        Color(
+            255,
+            95,
+            86
+        )
+    );
+
+    renderer.drawRect(
+        258,
+        136,
+        10,
+        10,
+        Color(
+            255,
+            189,
+            46
+        )
+    );
+
+    renderer.drawRect(
+        276,
+        136,
+        10,
+        10,
+        Color(
+            39,
+            201,
+            63
+        )
+    );
+
+    /* TERMINAL LINES */
+
+    int y = 190;
+
+    for (
+        int i = 0;
+        i < 14;
+        i++
+    ) {
+
+        renderer.drawRect(
+            260,
+            y,
+            440,
+            4,
+            Color(
+                90,
+                255,
+                120
+            )
+        );
+
+        y += 24;
+    }
 }
